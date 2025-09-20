e379bea4a1c2390e87ac3099c9f4ec4ea0c21d8e
shreemaan abhishek <shreemaan.abhishek@gmail.com>
2025-09-20 09:37:59 +0530

    prepare statements

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var input string
+	for {
+		fmt.Print("dbone > ")
+		fmt.Scan(&input)
+		if strings.HasPrefix(input, ".") {
+			if doMetaCommand(input) != 0 {
+				return
+			}
+			continue
+		}
+
+		prepareCommand(input)
+
+	}
+}
+
+func prepareCommand(statement string) {
+	command := strings.Fields(statement)[0]
+	switch command {
+	case "select":
+		fmt.Println("selecting...")
+	case "insert":
+		fmt.Println("inserting...")
+	default:
+		fmt.Println("unrecognized command")
+	}
+}
+
+func doMetaCommand(input string) int {
+	switch input {
+	case ".exit":
+		return -1
+	default:
+		fmt.Println("unrecognized command")
+		return 0
+	}
 }
