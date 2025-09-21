0cc028fddcf74b20b138470ac35a5652dac9318d
shreemaan abhishek <shreemaan.abhishek@gmail.com>
2025-09-21 11:35:27 +0530

    feat: select and insert

--- main.go
+++ main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"bufio"
+	"encoding/binary"
 	"fmt"
-	"strings"
+	"os"
 )
 
+const (
+	ID_SIZE       = 4
+	USERNAME_SIZE = 32
+	EMAIL_SIZE    = 255
+
+	ID_OFFSET       = 0
+	USERNAME_OFFSET = ID_OFFSET + ID_SIZE
+	EMAIL_OFFSET    = USERNAME_OFFSET + USERNAME_SIZE
+	ROW_SIZE        = EMAIL_OFFSET + EMAIL_SIZE
+
+	PAGE_SIZE       = 4096
+	ROWS_PER_PAGE   = PAGE_SIZE / ROW_SIZE
+	TABLE_MAX_PAGES = 100
+	TABLE_SIZE      = PAGE_SIZE * TABLE_MAX_PAGES
+)
+
+type Row struct {
+	id       uint32
+	username string
+	email    string
+}
+
+type Table struct {
+	numRows uint32
+	pages   [TABLE_SIZE]byte
+}
+
+var table Table
+
 func main() {
-	var input string
 	for {
 		fmt.Print("dbone > ")
-		fmt.Scan(&input)
-		if strings.HasPrefix(input, ".") {
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadBytes('\n')
+		if err != nil {
+			fmt.Println("failed to read from stdin")
+		}
+		if input[0] == '.' {
 			if doMetaCommand(input) != 0 {
 				return
 			}
@@ -22,24 +56,70 @@ func main() {
 	}
 }
 
-func prepareCommand(statement string) {
-	command := strings.Fields(statement)[0]
+func prepareCommand(statement []byte) *Row {
+	// TODO: validate the format of statement
+	statement_str := string(statement)
+	var row Row
+	var command string
+
+	fmt.Sscanf(statement_str, "%s %d %s %s", &command, &row.id, &row.email, &row.username)
+
 	switch command {
 	case "select":
 		fmt.Println("selecting...")
+		for i := 0; i < int(table.numRows); i++ {
+			row := rowSlot(uint32(i))
+			data := deserialise(table.pages[row:])
+			fmt.Println(data)
+		}
+
 	case "insert":
-		fmt.Println("inserting...")
+		slot := rowSlot(table.numRows)
+		fmt.Println("inserting @ ", slot)
+		serialisedRow := serialise(row)
+		copy(table.pages[slot:], serialisedRow)
+		table.numRows = table.numRows + 1
+		return &row
+
 	default:
 		fmt.Println("unrecognized command")
 	}
+	return &row
 }
 
-func doMetaCommand(input string) int {
-	switch input {
+func serialise(data Row) []byte {
+	bin := make([]byte, ROW_SIZE)
+	binary.LittleEndian.PutUint32(bin, data.id)
+	copy(bin[USERNAME_OFFSET:], data.username[:])
+	copy(bin[EMAIL_OFFSET:], data.email[:])
+
+	return bin
+}
+
+func deserialise(bin []byte) Row {
+	var row Row
+	row.id = binary.LittleEndian.Uint32(bin[ID_OFFSET:])
+	row.username = string(bin[USERNAME_OFFSET : USERNAME_OFFSET+USERNAME_SIZE])
+	row.email = string(bin[EMAIL_OFFSET : EMAIL_OFFSET+EMAIL_SIZE])
+
+	return row
+}
+
+func doMetaCommand(input []byte) int {
+	inputStr := string(input)
+	switch inputStr {
 	case ".exit":
 		return -1
 	default:
 		fmt.Println("unrecognized command")
 		return 0
 	}
+}
+
+func rowSlot(rowNum uint32) uint32 {
+	pageNum := rowNum / ROWS_PER_PAGE
+	rowOffset := rowNum % ROWS_PER_PAGE
+	byteOffset := rowOffset * ROW_SIZE
+
+	return pageNum + byteOffset
 }
